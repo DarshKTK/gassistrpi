8f0e06e764fabbf740afbf92ee8cccb27ecf0aec
Darsh K <53147930+DarshKTK@users.noreply.github.com>
2019-12-28 19:18:46 -0600

    Delete verify.sh

--- verify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source env/bin/activate
-google-oauthlib-tool --client-secrets /home/pi/Downloads/YOUR_DOWNLOADED_JSON_FILE_GOES_HERE --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
-exit 0
