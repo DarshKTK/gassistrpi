ef8017fe8d5e2a6e11d245a47521781d7eabccfb
Darsh K <53147930+DarshKTK@users.noreply.github.com>
2019-12-28 18:39:09 -0600

    update of sdk version

--- installer.sh
+++ installer.sh
@@ -7,6 +7,6 @@ sudo apt-get install python3-dev python3-venv -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools --upgrade
 source env/bin/activate
-python -m pip install --upgrade https://github.com/googlesamples/assistant-sdk-python/releases/download/0.3.0/google_assistant_library-0.0.2-py2.py3-none-linux_armv7l.whl
+python -m pip install --upgrade https://github.com/googlesamples/assistant-sdk-python/releases/download/0.5.1/google_assistant_sdk-0.5.1-py2.py3-none-any.whl
 python -m pip install --upgrade google-auth-oauthlib[tool]
 exit 0
